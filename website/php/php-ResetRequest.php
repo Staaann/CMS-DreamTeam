aa9c693100587c12cf1ec20c58fa5a47bdb8b129
jonathanabiy123 <46021232+jonathanabiy123@users.noreply.github.com>
2019-01-18 11:48:56 +0100

    eerste begin aan php

--- website/php/php-ResetRequest.php
+++ website/php/php-ResetRequest.php
@@ -1,10 +1,17 @@
 <?php 
+require('connect.php');
 
 if (isset($_POST["reset-reques-submit"])) {
 
-	# code...
-}else{
-	header("location: ")
-}
+	$selector =bin2hex(random_bytes(8));
+	$token= random_bytes(32);
 
+	$url = "http://localhost/CMS-DreamTeam/website/php/wachtwoord/wwvergetenpagina.php/selector=" .$selector . "&validator=" . bin2hex($token); 
+
+	$expires = date("U") + 1800; 
+
+}else{
+	header("location:index.php")
+}
+	
 

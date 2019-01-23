9cd14179c17aff5b346e44d45c2d7dcf65872479
NomisHaag <46443835+NomisHaag@users.noreply.github.com>
2019-01-23 13:54:53 +0100

    1

--- website/php/profile.php
+++ website/php/profile.php
@@ -17,11 +17,13 @@ if (!$_SESSION['username']) {
     <meta charset="utf-8">
     <title>Profile</title>
     <link rel="stylesheet" type="text/css" href="../css/profile.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
   <body>
     <header class="head_profile">
         <ul>
         <li><a href="#home">Home</a></li>
+        <li><a href="WebsiteStefan.php">Home</a></li>
         <li><a href="#news">Portofolio</a></li>
         <li><a href="#contact">Blog</a></li>
         <li><a href="#about">Chat</a></li>
@@ -29,12 +31,12 @@ if (!$_SESSION['username']) {
       </form>
       </ul>
     </header>
-
 <div class="wrapper_profile">
   <div class="welcome_profile">
+    <a href="WebsiteStefan.php" style="text-decoration: none; color:#0264ff;" >Homepage /</a>
     <?php
     //simon
-    echo "Profile, ";
+    echo "Profile / ";
     echo  $_SESSION['username']
     ?>
   </div>
@@ -43,10 +45,38 @@ Edit profile
 </div>
   <div class="edit_profile_body">
       <br>
-      <p class="edit_profile_body_text">Name:</p>
-      <br>
-      <p class="edit_profile_body_text">Email:</p>
-</div>
+        <ul>
+          <li><p class="edit_profile_body_text">Name</p></li>
+          <li><input class="edit_profile_vakjes" type="text" name="username"  placeholder="Username" required ></li>
+          <br>
+        </ul>
+      <ul>
+        <li><p class="edit_profile_body_text">Email</p></li>
+          <li> <input class="edit_profile_vakjes" type="email" name="email"  placeholder="Email" required></li> <br><br>
+        </ul>
+          <button  class="submit" type="submit" name="button" >Update</button>
+        </div>
+
+      <div class="edit_profile_title">
+        Change password
+      </div>
+        <div class="edit_profile_body">
+          <br>
+            <ul>
+              <li><p class="edit_profile_body_text">New password</p></li>
+              <li><input class="edit_profile_vakjes" type="text" name="username"  placeholder="Username" required ></li>
+            <br>
+          </ul>
+          <ul>
+        <li> <p class="edit_profile_body_text">Repeat password</p></li>
+          <li> <input class="edit_profile_vakjes" type="email" name="email"  placeholder="Email" required></li>
+           <br><br></ul>
+            <button  class="submit" type="submit" name="button" >Update</button>
+          </div>
+
+
+
+
   </div>
   </body>
 </html>

395d7c6623df7f74d87e1eaa8950bd8f670ee121
NomisHaag <46443835+NomisHaag@users.noreply.github.com>
2019-01-23 14:14:36 +0100

    blog veilig
    a

--- website/php/blog-index.php
+++ website/php/blog-index.php
@@ -1,4 +1,16 @@
+<?php
+require('connect.php');
 
+// Simon // make sure user is logged in needed every page!
+if (!$_SESSION['username']) {
+    $loginError = "You are not logged in.";
+    header("location: index.php");
+    exit();
+
+}
+      error_reporting(0);
+      ini_set('display_errors', 0)
+ ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,20 +19,20 @@
 </head>
 <body>
 <<<<<<< HEAD
-	
+
 <header>
 	<h1>Blog</h1>
 	<ul class="ul">
-						<a class="button, Simon" href="cat/cat1.php">Simon</a>  	    	
-						<a class="button, Stan" href="cat/cat2.php">Stan</a>      	    
-						<a class="button, Renko" href="cat/cat3.php">Renko</a> 
-						<a class="button, Jonathan" href="cat/cat4.php">Jonathan</a> 			
-						<a class="button, Stefan" href="cat/cat4.php">Stefan</a>  
-					
+						<a class="button, Simon" href="cat/cat1.php">Simon</a>
+						<a class="button, Stan" href="cat/cat2.php">Stan</a>
+						<a class="button, Renko" href="cat/cat3.php">Renko</a>
+						<a class="button, Jonathan" href="cat/cat4.php">Jonathan</a>
+						<a class="button, Stefan" href="cat/cat4.php">Stefan</a>
+
 
 </header>
-				
-				
+
+
 
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -28,11 +40,11 @@
 				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-				 
-=======
-	<header 
 
-	</header>  
+=======
+	<header
+
+	</header>
 
 	<div class="wrapper">
 
@@ -50,7 +62,7 @@
 				</ul>
 			</div>
 
-			
+
 	</div>
 
 >>>>>>> a138b1f9735906c011336f2866be763e36323062

21d7831fd4a1d8c4b02ab24cd40406dda6a26ef0
Staaann <Wiekstras@gmail.com>
2019-01-24 11:44:35 +0100

    uhhh idk
    Co-Authored-By: renkoo <renkoo@users.noreply.github.com>

---
+++ website/php/blog-test.php
@@ -0,0 +1,41 @@
+<?php 
+if (isset( $_POST['tekst'])){
+
+	$tekst = $_POST['tekst'];
+	var_dump($_POST);
+	echo $tekst;
+
+
+	require('connect.php');
+
+	$sql = "INSERT INTO blog (tekst) VALUES (:tekst)";
+    $query = $conn->prepare($sql);
+
+      $query->bindParam(':tekst', $username, PDO::PARAM_STR);
+      $query->execute();
+     }
+ ?>
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=5xpfjku55046lllgrobb2m0ru86f3gov7egzz8bru5ozabm4"></script>
+  <script>
+  tinymce.init({
+    selector: '#mytextarea'
+  });
+  </script>
+</head>
+
+<body>
+<h1></h1>
+  <form method="post">
+    <textarea name="tekst" id="mytextarea">Renko</textarea>
+    <button  class="submit" type="submit" name="button" >Test</button>
+  </form>
+
+</body>
+</html>

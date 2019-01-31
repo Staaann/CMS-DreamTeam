de874890452d28a13dc37b13dc0a9e070ee419ae
Staaann <Wiekstras@gmail.com>
2019-01-31 12:06:24 +0100

    Update viewpages.php
    Co-Authored-By: renkoo <renkoo@users.noreply.github.com>

--- website/php/viewpages.php
+++ website/php/viewpages.php
@@ -4,16 +4,9 @@ require ('connect.php');
 
 $pages = $conn->query("SELECT id, tekst FROM blog")->fetchAll(PDO::FETCH_ASSOC);
 
-<<<<<<< HEAD
 
-
-=======
-foreach($pages as $page){
-	//echo $page['tekst'];
-	//echo $page['id'];
-}
 require ('header.php')
->>>>>>> d46ae214bac66e3e497f33dc3c70a41d12960ce5
+
 ?>
 
 
@@ -22,7 +15,6 @@ require ('header.php')
 <html>
 <head>
 	<title>Vieuw pages</title>
-<<<<<<< HEAD
     <link rel="stylesheet" type="text/css" href="../css/grid.css">
 </head>
 <body>
@@ -33,21 +25,7 @@ require ('header.php')
 				//echo $page['id'];
 			}
 		?>	
-=======
 
-	<link rel="stylesheet" type="text/css" href="../css/grid.css">
-</head>
-<body>
-		<div class="test123">
-			<?php
-			foreach($pages as $page){
-			echo $page['tekst']; 
-			}
-			?>
-		</div>
-
-		
->>>>>>> d46ae214bac66e3e497f33dc3c70a41d12960ce5
 	</div>
 </body>
 </html>

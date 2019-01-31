7570f21d544d96f0c1e00ba7a120bfce75e25fbb
jonathanabiy123 <46021232+jonathanabiy123@users.noreply.github.com>
2019-01-31 20:09:23 +0100

    nknk

--- website/php/ww-request-mail.php
+++ website/php/ww-request-mail.php
@@ -1,5 +1,15 @@
 <?php
- require('connect.php');
+ if (isset($_POST['email'])) {
+ 	require_once 'wwconnect.php';
+
+  	$email = $conn-> real_escape_string($_POST['email']);
+
+  	$sql = $conn->query(query, "SELECT id FROM users WHERE email='$email'");
+  	if ($sql->num_rows > 0){
+
+  	}else
+  	  exit(json_decode(array("status" => 0, "msg"=> 'Pleas check your inputs!')));	
+ }
 
 
 ?>
@@ -18,11 +28,10 @@
 	<div>
 		<form action="php-ResetRequest.php" method="post">
 
-  			<input class="formulieremail" type="email1" placeholder="email" name="email" required>
+  			<input class="formulieremail" id="email" type="email" placeholder="email" name="email" required>
   				<br >
- 			<input class="loginknop " type="submit" name="reset-reques-submit" value ="send">
- 			<input class="loginknop" type="button" onclick="location.href='./index.php';" value="login" />
-
+ 			<input class="loginknop" type="submit" name="reset-reques-submit" value ="send">
+ 			
 		</form>
 
 	</div>

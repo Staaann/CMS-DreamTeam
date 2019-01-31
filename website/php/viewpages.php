a8c21479ac5c90387d559ea373567bded09efa44
Renko <Renkoannema01@gmail.com>
2019-01-31 12:01:00 +0100

    Blogvieuwpeages
    ASKLFJSAL;
    
    Co-Authored-By: stann <stann@users.noreply.github.com>

--- website/php/viewpages.php
+++ website/php/viewpages.php
@@ -5,10 +5,10 @@ require ('connect.php');
 $pages = $conn->query("SELECT id, tekst FROM blog")->fetchAll(PDO::FETCH_ASSOC);
 
 foreach($pages as $page){
-	echo $page['tekst'];
+	//echo $page['tekst'];
 	//echo $page['id'];
 }
-
+require ('header.php')
 ?>
 
 
@@ -17,8 +17,19 @@ foreach($pages as $page){
 <html>
 <head>
 	<title>Vieuw pages</title>
+
+	<link rel="stylesheet" type="text/css" href="../css/grid.css">
 </head>
 <body>
+		<div class="test123">
+			<?php
+			foreach($pages as $page){
+			echo $page['tekst']; 
+			}
+			?>
+		</div>
 
+		
+	</div>
 </body>
 </html>

5ba4f8b703d4fa04740528314d6d1228ee623133
jonathanabiy123 <46021232+jonathanabiy123@users.noreply.github.com>
2019-02-05 19:46:17 +0100

    ass

--- website/php/header.php
+++ website/php/header.php
@@ -64,7 +64,7 @@ if (!$_SESSION['username']) {
             ?>
       </p>
     </div>
-                      <div  style="float: left; margin: -30px 140px ; position: fixed; top: 36px; right:205px; border: 5px;">
+                      <div  style="float: left; margin: -30px 145px ; position: fixed; top: 36px; right:205px; border: 5px;">
 
 
     <img class="ProfilePicHok"  src="<?php echo $image; ?>" height='130' width='130'" >

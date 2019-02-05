3a14bcd11534bcdf0ec32444bafa3a97e5339cd8
NomisHaag <46443835+NomisHaag@users.noreply.github.com>
2019-02-05 18:49:22 +0100

    Betere aantekeningen

--- website/php/upload.php
+++ website/php/upload.php
@@ -4,7 +4,7 @@ $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
+//Hier word gekeken of het een echt of nep bestand is
 
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
@@ -16,23 +16,22 @@ if(isset($_POST["submit"])) {
         $uploadOk = 0;
     }
 }
-// Check if file already exists
 
-// Check file size
+// Maximale bestand grote
 if ($_FILES["fileToUpload"]["size"] > 1000000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
-// Allow certain file formats
+// White list bepaalde bestanden
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
 }
-// Check if $uploadOk is set to 0 by an error
+//
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
+// Alls alles goed is word de foto geupload
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
       $sql = 'UPDATE users SET image= "'.$target_file.'" WHERE username = "'.$_SESSION['username'].'"';
